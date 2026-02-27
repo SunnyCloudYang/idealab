b3741eea1b3eb5f89548bebd37202be3f0a69b40
SunnyCloudYang <sunnycloudyang@outlook.com>
2026-02-27 16:58:52 +0800

    chore: update base url

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/xxxlab/", // Add prefix to all static asset paths
+  base: "/idealab/", // Add prefix to all static asset paths
   build: {
     outDir: "docs",
     emptyOutDir: false,
