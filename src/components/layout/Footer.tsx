51f672512845071ae17446fd96449edbea97fce6
SunnyCloudYang <sunnycloudyang@outlook.com>
2026-08-19 23:37:52 +0800

    feat: update lab English name

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -26,8 +26,7 @@ const Footer: React.FC = () => {
                     智能决策博弈与数字经济创新
                   </h3>
                   <p className="text-gray-400 text-sm">
-                    Laboratory for Intellectual Decision-making in Engineering
-                    and Analytics
+                    Laboratory for Intelligent Decision-making with Emerging Algorithms
                   </p>
                 </div>
               </div>
